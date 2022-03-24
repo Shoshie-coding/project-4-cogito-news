8ac3ae953e344d1920d510dbeb2b6f641bff1263
Cristina Onea <cristinaonea86@gmail.com>
2022-03-24 00:25:33 +0000

    change allowed hosts on settings.py

--- cogito-news/settings.py
+++ cogito-news/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cogito_news.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['cogito-news.herokuapp.com', 'localhost']
 
 
 # Application definition
