536dfced6da35d6a444aa95a39b6cb1133335756
Cristina Onea <cristinaonea86@gmail.com>
2022-04-04 22:40:52 +0000

    Add basic styling and structure to categories

--- news/tests_url.py
+++ news/tests_url.py
@@ -1,11 +1,11 @@
-# from django.test import SimpleTestCase
-# from django.urls import reverse, resolve
-# from .views import *
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from news.views import generic, view
 
 
-# # Create your tests here
-# class TestMenusUrls(SimpleTestCase):
-#     def test_menus_url_is_resolved(self):
-#         url = reverse('menus')
-#         self.assertEquals(resolve(url).func, menus)
+# Create your tests here
+class TestMenusUrls(SimpleTestCase):
+    def test_create_url_is_resolved(self):
+        url = reverse('create')
+        self.assertEquals(resolve(url).func, PostCreateView)
 
