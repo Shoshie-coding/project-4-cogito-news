b21bb0b1601e2f0ebbe785919dd070220aaa79e4
Cristina Onea <cristinaonea86@gmail.com>
2022-04-07 17:30:49 +0000

    Change debug to development

--- cogito-news/settings.py
+++ cogito-news/settings.py
@@ -15,9 +15,7 @@ import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -30,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT'
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
